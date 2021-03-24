6a201a0aa2428167f78ecf1edbc4a03978e072e1
diganta15 <ddas461598@gmail.com>
2021-03-24 13:24:44 +0530

    Can search data about IP now

--- JS/main.js
+++ JS/main.js
@@ -6,14 +6,34 @@ button.addEventListener("click", searchIp);
 var API_KEY = "at_wGIUTvQdnN4lpS6grmPARPK9RvRjz";
 var dataVal = {};
 
-function searchIp() {
-  var val = ip.value;
+function setDefault() {
   fetch("https://geo.ipify.org/api/v1?apiKey=".concat(API_KEY, "&ipAddress=8.8.8.8\n")).then(function (res) {
     console.log(res);
     return res.text();
   }).then(function (data) {
     return createElements(JSON.parse(data).ip, JSON.parse(data).location, JSON.parse(data).isp);
   });
+}
+
+setDefault();
+
+function searchIp(e) {
+  e.preventDefault();
+  var val = ip.value;
+  console.log(val);
+
+  if (val !== "") {
+    fetch("https://geo.ipify.org/api/v1?apiKey=".concat(API_KEY, "&ipAddress=").concat(val, "\n")).then(function (res) {
+      console.log(res);
+      return res.text();
+    }).then(function (data) {
+      return createElements(JSON.parse(data).ip, JSON.parse(data).location, JSON.parse(data).isp);
+    });
+  } else {
+    setDefault();
+  }
+
+  val = "";
 }
 
 function createElements(ip, location, isp) {
@@ -23,4 +43,17 @@ function createElements(ip, location, isp) {
   console.log(location.lat);
   console.log(location.lng);
   console.log(isp);
+  appendText('ipAdd', ip);
+  appendText("location", location.city);
+  appendText("timezone", location.timezone);
+  appendText("isp", isp);
 }
+
+function appendText(element, text) {
+  document.getElementById(element).innerHTML = "";
+  var textContent = document.createTextNode(text);
+  var paragraph = document.getElementById(element);
+  paragraph.appendChild(textContent);
+}
+
+function createMap() {}
--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const API_KEY = "at_wGIUTvQdnN4lpS6grmPARPK9RvRjz";
 
 var dataVal = {};
 
-function searchIp() {
-  var val = ip.value;
 
+function setDefault(){
+ 
   fetch(`https://geo.ipify.org/api/v1?apiKey=${API_KEY}&ipAddress=8.8.8.8
 `)
     .then(function (res) {
@@ -22,9 +22,36 @@ function searchIp() {
         JSON.parse(data).isp,
       )
     );
-
-    
 }
+setDefault();
+ 
+function searchIp(e) {
+  e.preventDefault();
+  var val = ip.value;
+  console.log(val);
+
+ if(val!==""){
+    fetch(`https://geo.ipify.org/api/v1?apiKey=${API_KEY}&ipAddress=${val}
+`)
+      .then(function (res) {
+        console.log(res);
+        return res.text();
+      })
+      .then((data) =>
+        createElements(
+          JSON.parse(data).ip,
+          JSON.parse(data).location,
+          JSON.parse(data).isp
+        )
+      );
+ }
+ else{
+   setDefault();
+ }
+
+    val = "";
+}
+
 
 function createElements(ip, location, isp){
     console.log(ip);
@@ -33,4 +60,22 @@ function createElements(ip, location, isp){
     console.log(location.lat);
     console.log(location.lng)
     console.log(isp);
+
+    appendText('ipAdd',ip);
+    appendText("location", location.city);
+    appendText("timezone", location.timezone);
+    appendText("isp", isp);
+}
+
+function appendText(element, text){
+  document.getElementById(element).innerHTML = "";
+  var textContent = document.createTextNode(text);
+  var paragraph = document.getElementById(element);
+
+  paragraph.appendChild(textContent);
+
+}
+
+function createMap(){
+  
 }
